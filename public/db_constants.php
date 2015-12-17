be34e996ed6dab0bee3346e34b5c35227fad8947
Rajagopal Somasundaram <srgravi@gmail.com>
2015-12-17 13:37:46 +0000

    installed and updated the sentrifugo wrt brt

---
+++ public/db_constants.php
@@ -0,0 +1,7 @@
+<?php
+	           defined('SENTRIFUGO_HOST') || define('SENTRIFUGO_HOST','localhost');
+	           defined('SENTRIFUGO_USERNAME') || define('SENTRIFUGO_USERNAME','brtadmin');
+	           defined('SENTRIFUGO_PASSWORD') || define('SENTRIFUGO_PASSWORD','brt_HRM01@0#');
+	           defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','brthrm');
+	           
+	         ?>
