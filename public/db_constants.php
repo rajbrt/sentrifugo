469aa13daa2899d50123b419bcc78e8fec621b9c
rajbrt <rajagopal.s@blueracetechnologies.com>
2015-12-17 19:20:26 +0530

    updated the db connection details

--- public/db_constants.php
+++ public/db_constants.php
@@ -1,7 +1,7 @@
 <?php
 	           defined('SENTRIFUGO_HOST') || define('SENTRIFUGO_HOST','localhost');
-	           defined('SENTRIFUGO_USERNAME') || define('SENTRIFUGO_USERNAME','root');
-	           defined('SENTRIFUGO_PASSWORD') || define('SENTRIFUGO_PASSWORD','root');
-	           defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','brtportal');
+	           defined('SENTRIFUGO_USERNAME') || define('SENTRIFUGO_USERNAME','brthradmin');
+	           defined('SENTRIFUGO_PASSWORD') || define('SENTRIFUGO_PASSWORD','brt_HRM01@0#');
+	           defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','brthrm');
 	           
 	         ?>
